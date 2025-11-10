797da5bacfda7c62038e49ae58c4e7e2825581c8
User <jleo101007.2@gmail.com>
2025-11-10 11:05:52 +0200

    ver 2. 10/11/2025

---
+++ app/src/main/java/com/example/finalprojectleojacobovitz/Book.java
@@ -0,0 +1,80 @@
+package com.example.finalprojectleojacobovitz;
+
+public class Book {
+    private String nameOfBook;
+    private String authorsname;
+    private String uploadPagesCount;
+    private String uploadImageUrl;
+    private String uploadCategory;
+    private String uploadStartDate;
+    private String pagesread;
+
+    public String getPagesread() {
+        return pagesread;
+    }
+
+    public void setPagesread(String pagesread) {
+        this.pagesread = pagesread;
+    }
+
+    public String getNameOfBook() {
+        return nameOfBook;
+    }
+
+    public String getAuthorsname() {
+        return authorsname;
+    }
+
+    public String getUploadPagesCount() {
+        return uploadPagesCount;
+    }
+
+    public String getUploadCategory() {
+        return uploadCategory;
+    }
+
+    public String getUploadStartDate() {
+        return uploadStartDate;
+    }
+
+    public String getUploadImageUrl() {
+        return uploadImageUrl;
+    }
+
+    public void setNameOfBook(String nameOfBook) {
+        this.nameOfBook = nameOfBook;
+    }
+
+    public void setAuthorsname(String authorsname) {
+        this.authorsname = authorsname;
+    }
+
+    public void setUploadPagesCount(String uploadPagesCount) {
+        this.uploadPagesCount = uploadPagesCount;
+    }
+
+    public void setUploadImageUrl(String uploadImageUrl) {
+        this.uploadImageUrl = uploadImageUrl;
+    }
+
+    public void setUploadCategory(String uploadCategory) {
+        this.uploadCategory = uploadCategory;
+    }
+
+    public void setUploadStartDate(String uploadStartDate) {
+        this.uploadStartDate = uploadStartDate;
+    }
+
+    public Book(){
+    }
+    public Book(String nameOfBook, String authorsname, String uploadPagesCount, String uploadImageUrl, String uploadCategory, String uploadStartDate, String pagesread) {
+        this.nameOfBook = nameOfBook;
+        this.authorsname = authorsname;
+        this.uploadPagesCount = uploadPagesCount;
+        this.uploadImageUrl = uploadImageUrl;
+        this.uploadCategory = uploadCategory;
+        this.uploadStartDate = uploadStartDate;
+        this.pagesread = pagesread;
+    }
+
+}
