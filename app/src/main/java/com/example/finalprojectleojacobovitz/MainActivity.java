a25ff34a08fcf3c27545321c23141627ea214b19
USER <jleo101007.2@gmail.com>
2025-11-14 01:22:32 +0200

    ver 5. 14/11/2025

--- app/src/main/java/com/example/finalprojectleojacobovitz/MainActivity.java
+++ app/src/main/java/com/example/finalprojectleojacobovitz/MainActivity.java
@@ -4,21 +4,45 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button btnlogout,btnaddbook,btnlistbooks,btnAi;
+    Button btnlogout,btnaddbook,btnlistbooks,btnAi,btninstructions;
+    TextView name;
+    FirebaseAuth fauth;
+    FirebaseFirestore fstore;
+    String userId;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.name);
+        fauth = FirebaseAuth.getInstance();
+        fstore=FirebaseFirestore.getInstance();
+
+        userId = fauth.getCurrentUser().getUid();
+        DocumentReference documentReference = fstore.collection("users").document(userId);
+        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException error) {
+                name.setText("ברוך הבא: "+documentSnapshot.getString("fName"));
+            }
+        });
 
         btnlogout = findViewById(R.id.btnlogout);
         btnlogout.setOnClickListener(new View.OnClickListener() {
@@ -53,6 +77,14 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, AiActivity.class));
 
+            }
+        });
+
+        btninstructions = findViewById(R.id.btninstructions);
+        btninstructions.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, InstructionsActivity.class));
             }
         });
     }
