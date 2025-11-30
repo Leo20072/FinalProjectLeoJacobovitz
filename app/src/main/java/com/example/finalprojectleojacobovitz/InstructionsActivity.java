c25bfb65da4980cff0bdbf198002ab847c745f12
user <ron876864@gmail.com>
2025-11-30 15:55:36 +0200

    ver 11. 30/11/2025

--- app/src/main/java/com/example/finalprojectleojacobovitz/InstructionsActivity.java
+++ app/src/main/java/com/example/finalprojectleojacobovitz/InstructionsActivity.java
@@ -1,6 +1,8 @@
 package com.example.finalprojectleojacobovitz;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +12,22 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class InstructionsActivity extends AppCompatActivity {
 
+
+    Button btn_cta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_instructions);
+
+        btn_cta = findViewById(R.id.btn_cta);
+        btn_cta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
 
     }
 }
